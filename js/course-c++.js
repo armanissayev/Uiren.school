0f5c7e9070a9b8cd380d6b597f30c2e2aed17e71
Arman Issayev <issayevarman@mail.ru>
2023-11-06 21:13:38 +0600

    commit

--- js/course-c++.js
+++ js/course-c++.js
@@ -57,3 +57,99 @@ $(document).on("click", ".Variables-link", function(e) {
     $(".STL").hide();
     $(".Functions").hide();
 });
+
+$(document).on("click", ".Ifelse-link", function(e) {
+    $(".subtopic-name").text("/ If/else");
+    turnoff();
+    $(".Ifelse-link").css("text-decoration", "underline");
+
+    $(".Ifelse").show();
+    $(".IDE").hide();
+    $(".Code").hide();
+    $(".Variables").hide();
+    $(".Loops").hide();
+    $(".Static").hide();
+    $(".Dynamic").hide();
+    $(".STL").hide();
+    $(".Functions").hide();
+});
+
+$(document).on("click", ".Loops-link", function(e) {
+    $(".subtopic-name").text("/ Loops");
+    turnoff();
+    $(".Loops-link").css("text-decoration", "underline");
+
+    $(".Loops").show();
+    $(".IDE").hide();
+    $(".Code").hide();
+    $(".Ifelse").hide();
+    $(".Variables").hide();
+    $(".Static").hide();
+    $(".Dynamic").hide();
+    $(".STL").hide();
+    $(".Functions").hide();
+});
+
+$(document).on("click", ".Static-link", function(e) {
+    $(".subtopic-name").text("/ Static arrays");
+    turnoff();
+    $(".Static-link").css("text-decoration", "underline");
+
+    $(".Static").show();
+    $(".IDE").hide();
+    $(".Code").hide();
+    $(".Ifelse").hide();
+    $(".Variables").hide();
+    $(".Loops").hide();
+    $(".Dynamic").hide();
+    $(".STL").hide();
+    $(".Functions").hide();
+});
+
+$(document).on("click", ".Dynamic-link", function(e) {
+    $(".subtopic-name").text("/ Dynamic arrays");
+    turnoff();
+    $(".Dynamic-link").css("text-decoration", "underline");
+
+    $(".Dynamic").show();
+    $(".IDE").hide();
+    $(".Code").hide();
+    $(".Ifelse").hide();
+    $(".Variables").hide();
+    $(".Loops").hide();
+    $(".Static").hide();
+    $(".STL").hide();
+    $(".Functions").hide();
+});
+
+$(document).on("click", ".STL-link", function(e) {
+    $(".subtopic-name").text("/ STL");
+    turnoff();
+    $(".STL-link").css("text-decoration", "underline");
+
+    $(".STL").show();
+    $(".IDE").hide();
+    $(".Code").hide();
+    $(".Ifelse").hide();
+    $(".Variables").hide();
+    $(".Loops").hide();
+    $(".Static").hide();
+    $(".Dynamic").hide();
+    $(".Functions").hide();
+});
+
+$(document).on("click", ".Functions-link", function(e) {
+    $(".subtopic-name").text("/ Functions and Recursions");
+    turnoff();
+    $(".Functions-link").css("text-decoration", "underline");
+
+    $(".Functions").show();
+    $(".IDE").hide();
+    $(".Code").hide();
+    $(".Ifelse").hide();
+    $(".Variables").hide();
+    $(".Loops").hide();
+    $(".Static").hide();
+    $(".Dynamic").hide();
+    $(".STL").hide();
+});
